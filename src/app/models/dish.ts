160adcecf1fed92d6320298ebda8031b62e08592
Shaikh Ahmed Raza <shaikhahmedraza9@gmail.com>
2021-05-01 17:47:57 +0530

    added required properties

--- src/app/models/dish.ts
+++ src/app/models/dish.ts
@@ -1,16 +1,17 @@
 export class Dish {
-    public id? : number;
+    public id?: number;
     public name!: string;
     public description: string | undefined;
-    public price!: number;
-    // public brand: string,
-    // public brand: string,
+    public halfPrice!: number;
+    public fullPrice!: number;
     public qty!: number;
     public mainCategoryId?: number;
     public mainCategoryName: string;
     public isVeg:boolean;
     public isHalf:boolean;
-    // public picture?: string
+    public isFull:boolean;
+    public imageUrl:string;
+
     constructor(
     ) { }
 }
