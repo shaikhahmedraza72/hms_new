1498b514ee2a64e1a41a50f018ba7a7f778b8376
Shaikh Ahmed Raza <shaikhahmedraza9@gmail.com>
2021-05-04 17:01:07 +0530

    worked on checkbox selection

--- src/app/views/dish/dish.component.ts
+++ src/app/views/dish/dish.component.ts
@@ -8,24 +8,55 @@ import { DishService } from '../../service/dish.service';
   templateUrl: './dish.component.html'
 })
 export class DishComponent implements OnInit {
-
-  constructor(public dishSvc: DishService ) { }
-  dishList:Dish[] = [];
-  displayModel=false;
+  constructor(public dishSvc: DishService) { }
+  dishList: Dish[] = [];
+  isChecked: boolean;
+  selectedDish: number[] = [];
+  displayModel = false;
   ngOnInit(): void {
     this.loadData();
   }
-  showModel(){
+  showModel() {
     this.dishSvc.openModal();
   }
-  edit(id:number){
+  edit(id: number) {
     this.dishSvc.openModal();
     this.dishSvc.openEditModel(id);
   }
-  loadData(){
-    this.dishSvc.getList().subscribe(res=>{
+  loadData() {
+    this.dishSvc.getList().subscribe(res => {
       this.dishList = res;
-     });
+    });
   }
 
+  // onChkEdit(id){
+  //   if(this.isChecked === true){
+  //     this.selectedDish.push(id);
+  //   } else {
+  //     this.selectedDish.pop();
+  //   }
+  //   console.log(this.selectedDish); 
+
+  // }
+  onCheckboxChange(id, e) {
+    console.log(e.target.checked);
+    if (e.target.checked) {
+      if (this.selectedDish.indexOf(id) == -1) {
+        this.selectedDish.push(id);
+      }
+    } else {
+      const index = this.selectedDish.indexOf(id, 0);
+      if (index > -1) {
+        this.selectedDish.splice(index, 1);
+      }
+    }
+    console.log(this.selectedDish);
+  }
+
+
+  onDelete() {
+    if (this.selectedDish) {
+      this.selectedDish.pop();
+    }
+  }
 }
