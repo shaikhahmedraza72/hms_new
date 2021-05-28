4c73c34c8e5a93f185025e0ede205453ee90085d
Shaikh Ahmed Raza <shaikhahmedraza9@gmail.com>
2021-05-28 16:39:45 +0530

    alignments

--- src/app/views/dish/dish.component.ts
+++ src/app/views/dish/dish.component.ts
@@ -40,6 +40,7 @@ export class DishComponent implements OnInit {
     {label: 'Main Course', value: 'Main Course'},
     {label: 'Rice', value: 'Rice'}];
   }
+
   showModel() {
       this.dishSvc.openModal();
   }
