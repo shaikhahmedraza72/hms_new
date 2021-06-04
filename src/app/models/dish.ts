93c8e861c338e109a43afcdfee62a58ee0955847
rambhomkar <ram.bhomkar@gmail>
2021-06-04 20:09:04 +0530

    dish model change

--- src/app/models/dish.ts
+++ src/app/models/dish.ts
@@ -1,23 +1,22 @@
-export class Dish {
-    public id?: number;
-    public name!: string;
-    public description: string | undefined;
-    public halfPrice!: number;
-    public fullPrice!: number;
-    public qty!: number;
-    public image: any;
-    public mainCategoryId?: number;
-    public mainCategoryName: string;
-    public newCategory: string;
-    public isVeg:boolean;
-    public isHalf:boolean;
-    public isFull:boolean;
-    public imageUrl:any;
-    public nonVegCategory:string;
-    public cookingTime: any;
-    public minHr: string;
-    constructor(
-    ) { }
+export interface Dish {
+    id?: number;
+    name?: string;
+    description?: string | undefined;
+    halfPrice?: number;
+    fullPrice?: number;
+    qty?: number;
+    image?: any;
+    mainCategoryId?: number;
+    mainCategoryName?: string;
+    newCategory?: string;
+    isVeg?:boolean;
+    isHalf?:boolean;
+    isFull?:boolean;
+    imageUrl?:any;
+    nonVegCategory?:string;
+    cookingTime?: any;
+    minHr?: string;
+    
 }
 
 export class DishCategory{
