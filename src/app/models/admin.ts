b4034f02a5a82e7496de340d141273c20509db7f
Shaikh Ahmed Raza <shaikhahmedraza9@gmail.com>
2021-06-23 12:49:33 +0530

    commit

--- src/app/models/admin.ts
+++ src/app/models/admin.ts
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-export class Admin{
-  constructor(
-public businessName?:string,
-public category?: string,
-public gst?: string,
-public address?: string,
-public foodLincNum?: string,
-public id?: number,
-public pincode?: number,
-public restaurentLogo?:string,
-public restaurentSeal?:string,
-public signature?:string,
-public termAndCondition?:string,
-public bankDetails?: Bankdetails
-  ){}
-}
-export class Bankdetails {
-  constructor(
-    public accountName?: string,
-    public accountNumber?: number,
-    public bankName?: string,
-    public ifscCode?: string,
-    public UPICodeImg?:string,
-    public UPICodeNumber?:string){}
-=======
 export class Admin{
   constructor(
 public businessName?:string,
@@ -50,5 +24,4 @@ export class Bankdetails {
     public ifscCode?: string,
     public UPICodeImg?:string,
     public UPICodeNumber?:string){}
->>>>>>> e41bbbea95796848fa583b6ed59f059a62d067e4
   }
