304e2421009f036d11721e289d5bf3b3464344be
Shaikh Ahmed Raza <shaikhahmedraza9@gmail.com>
2021-07-02 16:48:55 +0530

    Admin changes

--- src/app/views/admin-setting/admin-setting.component.ts
+++ src/app/views/admin-setting/admin-setting.component.ts
@@ -15,8 +15,9 @@ export class AdminSettingComponent implements OnInit {
   @ViewChild('largeModal') public largeModal: ModalDirective;
   @ViewChild('f') form: any;
   admin: Admin;
-  states: { label: string; value: string; }[];
-  cities: { label: string; value: string; }[];
+  states: any;
+  cities: any;
+  selectedCategory:any;
   constructor(
     public clientSvc: ClientService,
     private msgService: MessageService,
@@ -37,6 +38,9 @@ export class AdminSettingComponent implements OnInit {
        const adminItm = resp[resp.length-1];
         adminItm.id = 1;
        this.admin = adminItm;
+       this.admin.categoryId = 2;
+       console.log(this.admin )
+       this.selectedCategory = this.admin.categoryId // { label:'business1', value:1};
       }
     })
     this.getClientCategory();
