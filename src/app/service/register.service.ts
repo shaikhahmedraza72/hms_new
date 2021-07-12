f6f79a9a9306fc0b001b2919db6cf7962da942a5
Shaikh Ahmed Raza <shaikhahmedraza9@gmail.com>
2021-07-12 18:17:41 +0530

    Register component

---
+++ src/app/service/register.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { ApiConfig } from '../constant/api';
+import { HttpClient } from '@angular/common/http';
+import { Registration } from '../models/registration';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/internal/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+  url = `${ApiConfig.URL}${ApiConfig.REGISTERED}`;
+  public register: Registration;
+  public registeredList: Registration[] = [];
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  AddUser(user: Registration): Observable<Registration> {
+    return this.http.post<Registration>(this.url, user).pipe(
+      map(x => {
+        this.registeredList.push(x);
+        return user;
+      }),
+      catchError(this.handleError('', user))
+    );
+  }
+
+  updateUser(user: Registration): Observable<Registration> {
+    return this.http.put<Registration>(`${this.url}`, user).pipe(
+      map(x => {
+        // tslint:disable-next-line:prefer-const
+        let index = this.registeredList.findIndex(i => i.id === x.id)
+        this.registeredList[index] = x;
+        return user;
+      }),
+      catchError(this.handleError('', user))
+    );
+  }
+
+  getRegisteredUser(): Observable<Registration[]> {
+    return this.http.get<Registration[]>(this.url).pipe(
+      map(x => {
+        this.registeredList = x;
+        return this.registeredList;
+      })
+    );
+  }
+
+
+  handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      //  this.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
+  }
+}
--- src/app/views/register/register.component.ts
+++ src/app/views/register/register.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, Type } from '@angular/core';
+import { Registration } from '../../models/registration';
+import { RegisterService } from '../../service/register.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: 'register.component.html'
 })
 export class RegisterComponent {
+  users: Registration;
+  submitted = false;
+  registeredList: Registration[] = [];
+  typeUser: { label: string; value: string; }[];
+  constructor(public regSvc: RegisterService) {
 
-  constructor() { }
 
+  }
+
+
+  ngOnInit(): void {
+    this.getUsers();
+    this.typeUser = [{
+      label: 'Admin', value: 'admin'
+    },
+    {
+      label: 'User', value: 'user'
+    }
+    ];
+  }
+
+
+
+  getUsers() {
+    this.regSvc.getRegisteredUser().subscribe(res => {
+      this.registeredList = res;
+    })
+  }
+
+
+  register() {
+    this.submitted = true;
+    this.regSvc.AddUser(this.users).subscribe(res => {
+      if (res) {
+        this.registeredList.push(this.users);
+        alert('Registration completed')
+      }
+    })
+  }
 }
