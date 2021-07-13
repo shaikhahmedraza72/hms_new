7ca423654aab1493d5662590ce6e5b764cc81af4
Shaikh Ahmed Raza <shaikhahmedraza9@gmail.com>
2021-07-13 17:36:15 +0530

    commit 13/7/2021

--- src/app/constant/api.ts
+++ src/app/constant/api.ts
@@ -1,20 +1,6 @@
 import {environment} from './../../environments/environment'
 export class ApiConfig {
     public static URL = environment.apiURL //'http://localhost:8000/'
-<<<<<<< HEAD
-    public static DISH = 'Dish'
-    public static DISHCATEGORY = 'DishCategory'
-    public static CLIENT = 'admin'
-    public static CLIENTCATEGORY = 'BusinessCategory'
-    public static BANKDETAILS = 'bankDetails'
-    public static ENDUSER = 'endUser'
-    public static USER = 'UserConfig'
-    public static CITY = 'StateCity/GetCity'
-    public static STATE = 'StateCity/GetState'
-    public static USERFEEDBACK = 'UserFeedback'
-    public static MASTERADMIN = 'masterAdmin'
-    public static REGISTERED = 'User'
-=======
     public static DISH = 'Dish';
     public static DISHCATEGORY = 'DishCategory';
     public static CLIENT = 'admin';
@@ -27,5 +13,4 @@ export class ApiConfig {
     public static USERFEEDBACK = 'UserFeedback';
     public static MASTERADMIN = 'masterAdmin';
     public static REGISTERED = "USER";
->>>>>>> 36d8a0025ea9d3c81b4b0a7de71883cda2f3c7d2
 }
