290cf630ea64d0dc8a3e3e088f97edf1f131e374
rambhomkar <ram.bhomkar@gmail>
2021-08-13 00:25:24 +0530

    billing button color change

--- src/app/views/dish/billing/billing.component.ts
+++ src/app/views/dish/billing/billing.component.ts
@@ -9,7 +9,7 @@ export class BillingComponent implements OnInit {
 
   stateOptions: any[];
   lblIsProceed: boolean;
-  paymentType: string;
+  paymentMode: string;
 
   constructor() { this.stateOptions = [{label: 'Off', value: 'off'}, {label: 'On', value: 'on'}];}
   value1: string = "off";
@@ -18,16 +18,16 @@ export class BillingComponent implements OnInit {
     console.log(this.value1);
   }
   fnCashProceed(v){
-    this.paymentType = "Cash"
+    this.paymentMode = "Cash"
     this.lblIsProceed = true;
   }
   fnUPIProceed(){
-    this.paymentType = 'UPI';
+    this.paymentMode = 'UPI';
     this.lblIsProceed = true;
   }
 
   fnResetPayment(){
-    this.paymentType = null;
+    this.paymentMode = null;
     this.lblIsProceed = false;
   }
    
