2739daa43140f1aa6b9c8ac43b1bf4c9f8a0935b
shaikhAhmedRaza <shaikhahmedraza9@gmail.com>
2021-08-21 17:58:41 +0530

    Barcode 21 Aug 2021

--- src/app/views/dish/billing/billing.component.ts
+++ src/app/views/dish/billing/billing.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { scan } from 'rxjs/internal/operators';
+import { Admin } from '../../../models/admin';
+import { AdminService } from '../../../service/admin.service';
 
 @Component({
   selector: 'app-billing',
@@ -12,10 +14,21 @@ export class BillingComponent implements OnInit {
   lblIsProceed: boolean;
   paymentMode: string;
   invoiceDialog: boolean;
-
-  constructor() { }
+  admin: Admin;
+  adminList: Admin[] = [];
+  constructor(public adminService: AdminService) { }
 
   ngOnInit(): void {
+    this.loadClient();
+  }
+
+  loadClient(){
+    this.adminService.getClientList().subscribe(resp => {
+      if(resp.length > 0){  
+       const adminItm = resp.find(x => x.id == x.id);
+       this.admin = adminItm;    
+      }
+    });
   }
   fnCashProceed(v){
     this.paymentMode = "Cash"
